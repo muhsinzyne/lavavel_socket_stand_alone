c6b6a020e680c07483968f2a933eea3e4bb781b4
Muhsin Zyne <muhsin.zyne@gmail.com>
2020-09-20 10:55:51 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('sockets/serve', function(){
+    \Illuminate\Support\Facades\Artisan::call('websockets:serve');
+    echo "socket started";
+});
