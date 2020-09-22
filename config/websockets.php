677f6334e03bfa1693bc9bc02ae8f33a52d45750
Muhsin Zyne <muhsin.zyne@gmail.com>
2020-09-22 22:39:37 +0300

    Update websockets.php

--- config/websockets.php
+++ config/websockets.php
@@ -115,22 +115,22 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => '/home/goit/goit-socket.ml/ssl/certificate.crt',
-         //'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        //'local_cert' => '/home/goit/goit-socket.ml/ssl/certificate.crt',
+         'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
 
          /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
         
-        'local_pk' => '/home/goit/goit-socket.ml/ssl/private.key',
-        //'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        //'local_pk' => '/home/goit/goit-socket.ml/ssl/private.key',
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
 
         /*
          * Passphrase for your local_cert file.
          */
         'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
-        'verify_peer' => false,
+        //'verify_peer' => false,
 
     ],
 
